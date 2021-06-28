a593b426ababd847c9f25f7fa5297f572c22375b
Denis Lyakhovich <>
2021-06-28 15:48:32 +0300

    Применение кроссбраузерности и паттерна фабрика для вызова разных браузеров.

--- src/test/java/ExampleTest.java
+++ src/test/java/ExampleTest.java
@@ -1,27 +1,59 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-public class ExampleTest {
+public class ExampleTest  {
     protected static WebDriver driver;
-    private Logger logger = LogManager.getLogger(ExampleTest.class);
+    private final Logger logger = LogManager.getLogger(ExampleTest.class);
+
+    //читаем передаваемый праметр browser по ключ-значению
+    String env = System.getProperty("browser", "chrome");
 
     @BeforeEach
     public void setUp() {
-        WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        logger.info("Драйвер стартовал!");
+        logger.info("env = " + env);
+        driver = WebDriverFactory.getDriver(env.toLowerCase());
+        logger.info("Драйвер стартовал");
+        //WebDriverManager.chromedriver().setup();
+        //driver = new ChromeDriver();
+        //logger.info("Драйвер стартовал!");
     }
 
     @Test
     public void openPage() {
         driver.get("https://yandex.ru/");
         logger.info("Открыта страница Yandex - " + "https://yandex.ru/");
+
+        //выводим заголовлок страцицы  методом .getTitle()
+        String title = driver.getTitle();
+        logger.info("Title - ", title.toString());
+
+        //вывод URL методом .getCurrentUrl()
+        String currentURL = driver.getCurrentUrl();
+        logger.info("CurrentURL - ", currentURL.toString());
+
+        //ввод текста для строки поиска
+        String searchInputPath = ".//input[@class='input__control input__input mini-suggest__input']";
+        WebElement searchInput = driver.findElement(By.xpath(searchInputPath));
+        String searchText = "QA Automation";
+        searchInput.sendKeys(searchText);
+
+        //нажатие кнопки найти
+        String searchButtonXpath = ".//button[@class='button mini-suggest__button button_theme_search button_size_search i-bem button_js_inited']";
+        WebElement searchButton = driver.findElement(By.xpath(searchButtonXpath));
+        searchButton.click();
+
+        //задержка выполнения для отслеживания результата
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        };
     }
 
     @AfterEach
---
+++ src/test/java/WebDriverFactory.java
@@ -0,0 +1,40 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class WebDriverFactory {
+
+    private static final Logger logger =  LogManager.getLogger(WebDriverFactory.class);
+
+    public static WebDriver getDriver (String browserName){
+        switch (browserName){
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                logger.info("Драйвер для браузера Chrome");
+                return new ChromeDriver();
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                logger.info("Драйвер для браузера Firefox");
+                return new FirefoxDriver();
+            case "safari":
+                logger.info("Драйвер для браузера Safari");
+                return new SafariDriver();
+            case "edge":
+                WebDriverManager.edgedriver().setup();
+                logger.info("Драйвер для браузера Edge");
+                return new EdgeDriver();
+            case "opera":
+                WebDriverManager.operadriver().setup();
+                logger.info("Драйвер для браузера Opera");
+                return  new OperaDriver();
+            default:
+                throw new RuntimeException("Incorrect browser name");
+        }
+    }
+}
